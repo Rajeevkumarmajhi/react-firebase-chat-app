a4dceec0008aeeb5feb927cfe7e02d193f905d36
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2022-12-29 22:52:01 +0545

    -->initialized firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiAMDi9n6Bw1RjoJ5HS6e54sVJ8qFj550",
+  authDomain: "chat-d07fe.firebaseapp.com",
+  projectId: "chat-d07fe",
+  storageBucket: "chat-d07fe.appspot.com",
+  messagingSenderId: "674807263015",
+  appId: "1:674807263015:web:8417725477ac22a9e34302"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
