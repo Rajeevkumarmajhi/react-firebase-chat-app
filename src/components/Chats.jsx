da654d087464c2c9544cc043a438ad174afc48d1
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-01-02 21:55:23 +0545

    fixed the side bar search user section and get realtime live data on chat conversations

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { db } from '../firebase';
 
 const Chats = () => {
+  const [chats,setChats] = useState([]);
+  const {currentUser} = useContext(AuthContext)
+  
+  useEffect(()=>{
+
+    const getChats = () =>{
+      
+          const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+              // console.log("Current data: ", doc.data());
+              setChats(doc.data());
+          });
+      
+          return () =>{
+            unsub();
+          }
+    }
+
+    currentUser.uid && getChats()
+
+  },[currentUser.uid]);
+
+  console.log(chats);
+
   return (
     <div className='chats'>
-      <div className="userChat">
-        <img src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar' />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>hello</p>
-        </div>
-      </div> 
-      <div className="userChat">
-        <img src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar' />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>hello</p>
-        </div>
-      </div> 
-      <div className="userChat">
-        <img src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar' />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>hello</p>
-        </div>
-      </div> 
-      <div className="userChat">
-        <img src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar' />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>hello</p>
-        </div>
-      </div> 
+      { Object.entries(chats)?.map((chat) => ( 
+        <div className="userChat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt='avatar' />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastmessage?.text}</p>
+          </div>
+        </div> 
+      ))}
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,88 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { db } from "../firebase"
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore";
+import { AuthContext }  from "../context/AuthContext";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+  const { currentUser } = useContext(AuthContext)
+
+  const handleSearch = async() => {
+
+    const q = query(collection(db,"users"),where("displayName","==",username));
+
+    try{
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
+
+    }catch(err){
+      setErr(true);
+    }
+  }
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  }
+
+  const handleSelect = async (e) => {
+    // check if conversations (chats in firestore) exists or not , if not create one
+    const combinedId = currentUser.uid >user.id ? currentUser.uid + user.uid : user.uid + currentUser.uid
+    
+    try{
+      const res = await getDoc(doc(db,'chats', combinedId))
+
+      if(!res.exists()){
+        // create a chat in chat collections
+        await setDoc(doc(db,"chats",combinedId),{messages:[]})
+
+        // create user chats
+        await updateDoc(doc(db,"userChats",currentUser.uid),{
+          [combinedId+".userInfo"]:{
+            uid:user.uid,
+            displayName:user.displayName,
+            photoURL:user.photoURL
+          },
+          [combinedId+".date"]:serverTimestamp()
+        });
+        await updateDoc(doc(db,"userChats",user.uid),{
+          [combinedId+".userInfo"]:{
+            uid:currentUser.uid,
+            displayName:currentUser.displayName,
+            photoURL:currentUser.photoURL
+          },
+          [combinedId+".date"]:serverTimestamp()
+        });
+      }
+
+    }catch(err){
+
+      setUser(null);
+      setUsername("");
+    }
+
+    // create user chats
+
+  }
+
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='Find a user' />
+        <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={(e)=>setUsername(e.target.value)} value={username} />
       </div>
-     <div className="userChat">
-        <img src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar' />
+      {err && <span>User not found! </span>}
+     {user && <div className="userChat" onClick={handleSelect}>
+        <img src={user.photoURL} alt='avatar' />
         <div className="userChatInfo">
-          <span>Jane</span>
+          <span>{user.displayName}</span>
         </div>
-      </div> 
+      </div>} 
     </div>
   )
 }
